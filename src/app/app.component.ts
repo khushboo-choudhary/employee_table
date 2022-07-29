eb9338d3bd7fa242fa2c0de46d69250d04645ca0
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2022-07-29 20:44:11 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms'
+import { Employee } from 'src/app/model/employee';
+import { EmployeeService } from 'src/app/service/employee.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employee_table';
+export class AppComponent implements OnInit {
+  title= "Employee table";
+  empDetail !: FormGroup;
+  empObj : Employee = new Employee();
+  empList : Employee[] = [];
+
+  constructor(private formBuilder : FormBuilder, private empService : EmployeeService) { }
+
+  ngOnInit(): void {
+    
+
+    this.getAllEmployee();
+
+    this.empDetail = this.formBuilder.group({
+      // id : [''],
+      name : [''],
+      salary: [''],
+      email: ['']
+    });    
+
+  }
+
+  addEmployee() {
+    console.log("getDetails",this.empDetail);
+    // this.empObj.id = this.empDetail.value.id;
+    this.empObj.name = this.empDetail.value.name;
+    this.empObj.salary = this.empDetail.value.salary;
+    this.empObj.email = this.empDetail.value.email;
+
+    this.empService.addEmployee(this.empObj).subscribe(res=>{
+        console.log(res);
+        this.getAllEmployee();
+    },err=>{
+        console.log(err);
+    });
+
+  }
+
+  getAllEmployee() {
+    this.empService.getAllEmployee().subscribe(res=>{
+        this.empList = res;
+    },err=>{
+      console.log("error while fetching data.")
+    });
+  }
+
+  editEmployee(emp : Employee) {
+    this.empDetail.controls['id'].setValue(emp.id);
+    this.empDetail.controls['name'].setValue(emp.name);
+    this.empDetail.controls['email'].setValue(emp.email);
+    this.empDetail.controls['salary'].setValue(emp.salary);
+
+  }
+
+  updateEmployee() {
+
+    this.empObj.id = this.empDetail.value.id;
+    this.empObj.name = this.empDetail.value.name;
+    this.empObj.salary = this.empDetail.value.salary;
+    this.empObj.email = this.empDetail.value.email;
+
+    this.empService.updateEmployee(this.empObj).subscribe(res=>{
+      console.log(res);
+      this.getAllEmployee();
+    },err=>{
+      console.log(err);
+    })
+
+  }
+
+  deleteEmployee(emp : Employee) {
+
+    this.empService.deleteEmployee(emp).subscribe(res=>{
+      console.log(res);
+      alert('Employee deleted successfully');
+      this.getAllEmployee();
+    },err => {
+      console.log(err);
+    });
+
+  }
+
 }
